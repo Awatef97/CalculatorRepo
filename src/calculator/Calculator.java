629fa0d8d43bc9636764dbcce93a0c2f2a1e085a
awatef <awatef.ahmed97@gmail.com>
2020-02-13 15:47:42 +0200

    first commit

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+/**
+ *
+ * @author awatef
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    //awtef start
+    
+    //awatef end
+    //ziad start
+    
+    //ziad end
+    //wagdy start
+    
+    //wagdy end
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //awtef start
+        
+        //awatef end
+        //ziad start
+        
+        //ziad end
+        //wagdy start
+        
+        //wagdy end
+        
+    }
+    
+}
