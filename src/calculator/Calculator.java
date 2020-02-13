50ab7d7b3748a6042dd72002e7fe7203c305fa9a
AhmedWagdy <a7med.wagdy96@gmail.com>
2020-02-13 17:53:15 +0200

    4th commit

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -21,7 +21,9 @@ public class Calculator {
     
     //ziad end
     //wagdy start
-    
+    public static void multi(int num1, int num2) {
+        System.out.println(num1*num2);
+    }
     //wagdy end
    
     public static void main(String[] args) {
@@ -34,7 +36,7 @@ public class Calculator {
         
         //ziad end
         //wagdy start
-        
+        multi(2,3);
         //wagdy end
         
     }
